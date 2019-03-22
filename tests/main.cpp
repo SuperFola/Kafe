2984c3e0988a2e63cee534c363bf3d17e99d24cd
Alexandre Plateau <lexplt.dev@gmail.com>
2019-03-22 15:06:18 +0100

    enhancing tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,73 +1,40 @@
-// to print stuff and parse use Kafe
+#include "main.hpp"
+
 #include <iostream>
 #include <kafe/kafe.hpp>
-
-// to read files
-#include <string>
-#include <fstream>
-#include <streambuf>
-
-// for the tests (ASTtoString)
 #include <sstream>
-
-std::string readFile(const std::string& name)
-{
-    std::ifstream f(name.c_str());
-    // admitting the file exists
-    return std::string(
-        (std::istreambuf_iterator<char>(f)),
-        std::istreambuf_iterator<char>()
-    );
-}
-
-bool deepCompareString(const std::string& a, const std::string& b)
-{
-    std::string line = "";
-    std::size_t index = 0;
-    std::size_t row = 1;
-
-    for (std::size_t i=0; i < a.size(); ++i)
-    {
-        if (i >= b.size())
-        {
-            std::cout << "b is too short" << std::endl;
-            return false;
-        }
-
-        if (b[i] == '\n')
-        {
-            line = "";
-            index = 0;
-            ++row;
-        }
-        else
-        {
-            line += b[i];
-            ++index;
-        }
-        
-        if (a[i] != b[i])
-        {
-            std::cout << line << " (line: " << row << ")\n";
-            if (index > 0)
-            {
-                for (std::size_t j=0; j < index - 1; ++j)
-                    std::cout << "~";
-            }
-            std::cout << "^" << std::endl;
-            return false;
-        }
-    }
-    return true;
-}
+#include <filesystem>
+#include <string>
 
 int main()
 {
-    std::cout << "testing Kafe" << std::endl;
+    std::cout << "Kafe tests" << std::endl;
 
+    // getting all the tests in the directory kafe/
+    std::vector<std::string> v;
+    std::filesystem::path p("./kafe/");
+    std::filesystem::directory_iterator start(p);
+    std::filesystem::directory_iterator end;
+    std::transform(start, end, std::back_inserter(v), path_leaf_string());
+
+    // only keep the .kafe
+    std::vector<std::string> files;
+    for (auto f: v)
+    {
+        if (f.size() > 8 && f.substr(f.size() - 4) == "kafe")
+            files.push_back(f);
+    }
+
+    std::size_t i = 0;
+    std::size_t passed = 0;
+    std::size_t failed = 0;
+
+    // testing each file :
+    //    comparing generated AST and output
+    for (auto file: files)
     {
         // parsing
-        kafe::Parser p(readFile("kafe/def.kafe"));
+        kafe::Parser p(readFile(file));
         p.parse();
 
         // getting AST
@@ -75,13 +42,22 @@ int main()
         p.ASTtoString(os);
 
         // comparing with what we need to have
-        auto content = readFile("kafe/def.kafe.expected");
-        bool passed = deepCompareString(os.str(), content);
-        if (passed)
-            std::cout << "Passed" << std::endl;
+        auto content = readFile(file + ".expected");
+        
+        if (deepCompareString(os.str(), content))
+            ++passed;
         else
-            std::cout << "Failed" << std::endl;
+        {
+            ++failed;
+            std::cout << "Failed test " << i << std::endl;
+        }
+        
+        ++i;
     }
+
+    std::cout << std::endl << std::endl
+        << "Tests passed: " << passed << "/" << i << std::endl
+        << "Tests failed: " << failed << "/" << i << std::endl;
 
     return 0;
 }
---
+++ tests/main.hpp
@@ -0,0 +1,70 @@
+#ifndef main_hpp
+#define main_hpp
+
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+#include <filesystem>
+#include <algorithm>
+
+struct path_leaf_string
+{
+    std::string operator()(const std::filesystem::directory_entry& entry) const
+    {
+        return entry.path().string();
+    }
+};
+
+inline std::string readFile(const std::string& name)
+{
+    std::ifstream f(name.c_str());
+    // admitting the file exists
+    return std::string(
+        (std::istreambuf_iterator<char>(f)),
+        std::istreambuf_iterator<char>()
+    );
+}
+
+inline bool deepCompareString(const std::string& a, const std::string& b)
+{
+    std::string line = "";
+    std::size_t index = 0;
+    std::size_t row = 1;
+
+    for (std::size_t i=0; i < a.size(); ++i)
+    {
+        if (i >= b.size())
+        {
+            std::cout << "b is too short" << std::endl;
+            return false;
+        }
+
+        if (b[i] == '\n')
+        {
+            line = "";
+            index = 0;
+            ++row;
+        }
+        else
+        {
+            line += b[i];
+            ++index;
+        }
+        
+        if (a[i] != b[i])
+        {
+            std::cout << line << " (line: " << row << ")\n";
+            if (index > 0)
+            {
+                for (std::size_t j=0; j < index - 1; ++j)
+                    std::cout << "~";
+            }
+            std::cout << "^" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+#endif
